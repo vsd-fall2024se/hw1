a934ef390c8efff4e4ad29f25441d7d2c816ac10
Vatsal Patel <75309827+vatsal-dp@users.noreply.github.com>
2024-08-29 00:30:43 +0000

    final

--- test_myfile.py
+++ test_myfile.py
@@ -3,5 +3,5 @@ from myfile import addition
 def test_correct():
     assert addition(3,4) == 7
 
-# def test_incorrect():
-    # assert addition(3,4) == 8
+def test_incorrect():
+    assert addition(3,4) == 8
